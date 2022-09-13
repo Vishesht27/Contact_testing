81c5ae66f71851974478da72294450a5abddcba7
Vishesh <vishesht27@gmail.com>
2022-09-13 17:54:07 +0530

    backend connection

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import uba_logo from "./uba_logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import ContactForm from "./contactForm";
 
 function App() {
+  // You can skip useEffect if you're not using TailwindCSS
+  // Otherwise, for the production usage refer to https://tailwindcss.com/docs/installation
+  useEffect(() => {
+    if (document) {
+      const stylesheet = document.createElement("link");
+      stylesheet.rel = "stylesheet";
+      stylesheet.href = "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css";
+
+      document.head.appendChild(stylesheet);
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={uba_logo} alt="logo" />
+        <div className="py-6">
+          <ContactForm />
+        </div>
       </header>
     </div>
   );
---
+++ src/contactForm.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+
+const FORM_ENDPOINT = "https://public.herotofu.com/v1/049d6450-12f3-11ed-bc91-695741f28ee9";
+
+const ContactForm = () => {
+    const [submitted, setSubmitted] = useState(false);
+    const handleSubmit = () => {
+        setTimeout(() => {
+            setSubmitted(true);
+        }, 100);
+    };
+
+    if (submitted) {
+        return (
+            <>
+                <div className="text-2xl">Thank you!</div>
+                <div className="text-md">We'll be in touch soon.</div>
+            </>
+        );
+    }
+
+    return (
+        <form
+            action={FORM_ENDPOINT}
+            onSubmit={handleSubmit}
+            method="POST"
+            target="_blank"
+        >
+            <div className="mb-3 pt-0">
+                <input
+                    type="text"
+                    placeholder="Your First name"
+                    name="name"
+                    className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                    required
+                />
+            </div>
+            <div className="mb-3 pt-0">
+                <input
+                    type="text"
+                    placeholder="Your Last name"
+                    name="name"
+                    className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                    required
+                />
+            </div>
+
+            <div className="mb-3 pt-0">
+                <input
+                    type="email"
+                    placeholder="Your Email Address"
+                    name="email"
+                    className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                    required
+                />
+            </div>
+
+            <div className="mb-3 pt-0">
+                <input
+                    type="number"
+                    placeholder="Your Phone Number"
+                    name="phone"
+                    className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                    required
+                />
+            </div>
+
+            <div className="mb-3 pt-0">
+                <input
+                    type="text"
+                    placeholder="Are you new Client"
+                    name="client"
+                    className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                    required
+                />
+            </div>
+
+            <div className="mb-3 pt-0">
+                <textarea
+                    type = "text"
+                    placeholder="Your message"
+                    name="message"
+                    className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                    required
+                />
+            </div>
+            
+            <div className="mb-3 pt-0">
+                <button
+                    className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="submit"
+                >
+                    Send a message
+                </button>
+            </div>
+        </form>
+    );
+};
+
+export default ContactForm;
